bfe429f6bf214dc3fa7bf756544422a4eae04e7b
Abramov Egor <abramov.egor940528@icloud.com>
2024-07-21 17:22:54 +0300

    добалили Лейбл даты и лейбл дня недели

--- daysOfTheWeek/ViewController.swift
+++ daysOfTheWeek/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private struct DataSource {
+        let day: String
+        let data: String
+    }
     
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -17,38 +21,78 @@ class ViewController: UIViewController {
         return tableView
     }()
     
-    let daysOfWeek: [String ] = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
+    private let dataSource: [DataSource] = [
+        DataSource(day: "Понедельник", data: "01.07"),
+        DataSource(day: "Вторник", data: "02.07"),
+        DataSource(day: "Среда", data: "03.07"),
+        DataSource(day: "Четверг", data: "04.07"),
+        DataSource(day: "Пятница", data: "05.07"),
+        DataSource(day: "Суббота", data: "06.07"),
+        DataSource(day: "Воскресенье", data: "07.07"),
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         tableView.delegate = self
         tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-       
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        
     }
     private func setupView() {
         view.backgroundColor = .systemBackground
-        
         view.addSubview(tableView)
-        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
-        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
-        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(
+            equalTo: view.leadingAnchor
+        ).isActive = true
+        tableView.trailingAnchor.constraint(
+            equalTo: view.trailingAnchor
+        ).isActive = true
+        tableView.topAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.topAnchor
+        ).isActive = true
+        tableView.bottomAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.bottomAnchor
+        ).isActive = true
     }
-
 }
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return daysOfWeek.count
-    }
+    extension ViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { 134 }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return dataSource.count
+        }
+    
+
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = daysOfWeek[indexPath.row]
-    
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? TableViewCell {
+//            cell.dayLabel.text = daysOfWeek[indexPath.row]
+//            cell.dateLabel.text = dates[indexPath.row]
+//            
+//            return cell
+//        }
+//        
+//        return UITableViewCell()
+//        
+//        if создание ячейки {
+//            настройка ячейки
+//            return ячейка
+//        }
+//        return базовая ячейка
+//        
+//        guard создание ячейки else {return базовая ячейка}
+//        настройка ячейки
+//        return ячейка
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? TableViewCell else {return UITableViewCell()}
+        cell.configCell(
+            dayName: dataSource[indexPath.row].day,
+            dateName: dataSource[indexPath.row].data
+        )
+        
         return cell
     }
-    
-    
 }
